313112b60369812c4cdb150db99c0ce65ca9a32c
Artur <artur@pragacz.com>
2019-09-06 04:20:02 +0200

    0.4c

--- script.ts
+++ script.ts
@@ -1,28 +1,89 @@
-// arranger = {
-// 	getArrangement,
-// 	setArrangement,
-// 	onArrangementChanged,
-// 	startConnection
-// }
 
-window.storage.init().then(() => {
+var running = false;
+// var operational = false; // TODO
 
-	let runningConnection = false;
+var current: ArrangementStore;
+var backupTimerIds: number[] = [];
 
-	browser.browserAction.onClicked.addListener(function () {
-		if (runningConnection) {
-			arranger.stopConnection();
-		}
-		else {
-			arranger.startConnection();
-		}
-		runningConnection = !runningConnection;
-	})
+function windowCreated(wndw: browser.windows.Window) {
+	console.log("windowCreated: ", wndw);
+}
 
-	arranger.onArrangementChanged.addEventListener("shuffled", function (e) {
-		console.log("shuffled: ", e);
-	})
+function windowRemoved(wndwId: number) {
+	const windowRemovedOi = new ObserveInfo().delete(wndwId);
+	storager.changeObserved(windowRemovedOi)
+}
 
-	// arranger.startConnection();
+function windowsRearranged(e: CustomEvent) {
+	const arrangementUpdate: Arrangement = e.detail;
+	const arrangementStoreUpdate = new ArrangementStore(arrangementUpdate);
+	current = mergeArrangementStores(current, arrangementStoreUpdate);
+	storager.saveArrangementStore("$current", current);
+}
 
-});
+
+async function mainStart() {
+	if (!running) {
+		running = true;
+		// operational = false;
+
+		const allWindows = await browser.windows.getAll() // TODO?: add devtool windows?
+		const allWindowIds = allWindows.map(w => w.id);
+		const allWindowOi = new ObserveInfo().add(allWindowIds);
+
+		await storager.start();
+		arranger.startConnection();
+
+		await storager.changeObserved(allWindowOi);
+		const arrangement = await arranger.changeObserved(allWindowOi);
+		current = new ArrangementStore(arrangement);
+
+		await storager.copyArrangementStore("$current", "$previous").catch(() => {});
+		await storager.saveArrangementStore("$current", current);
+
+		browser.windows.onCreated.addListener(windowCreated);
+		browser.windows.onRemoved.addListener(windowRemoved);
+		arranger.onArrangementChanged.addEventListener("arrangementChanged", windowsRearranged);
+
+		// operational = false;
+	}
+	else
+		console.log("Main already running!");
+}
+
+function mainStop() {
+	if (running) {
+		running = false;
+
+		storager.stop();
+		arranger.stopConnection();
+
+		arranger.onArrangementChanged.removeEventListener("arrangementChanged", windowsRearranged);
+		browser.windows.onCreated.removeListener(windowCreated);
+		browser.windows.onRemoved.removeListener(windowRemoved);
+	}
+	else
+		console.log("No running Main!");
+}
+
+browser.browserAction.onClicked.addListener(function () {
+	if (running) {
+		mainStop();
+	}
+	else {
+		mainStart();
+	}
+	// running = !running;
+})
+
+
+async function loadFromMemory(name: string) {
+
+}
+
+async function clear() {
+	const allWindows = await browser.windows.getAll() // TODO?: add devtool windows?
+	const allWindowIds = allWindows.map(w => w.id);
+	allWindowIds.asyncForEach(id=>browser.sessions.removeWindowValue(id, "uid"));
+	browser.storage.local.clear();
+}
